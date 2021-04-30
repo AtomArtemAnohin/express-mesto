24181c6375048bab2529d3e289628005af7f6cb2
AtomArtemAnohin <takleg4e@gmail.com>
2021-04-30 07:30:11 +0300

    Final

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '60891554b485c06a66cc2357',
+  };
+  next();
+});
+app.use('/', userRouter);
+app.use('/', cardRouter);
+
+app.listen(PORT);
