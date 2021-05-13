c771dc43cf24f0ab98d62a6d7db82a80f951b4b6
AtomArtemAnohin <takleg4e@gmail.com>
2021-05-13 12:18:53 +0300

    незнаяительные исправления

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 
@@ -20,21 +21,28 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email(),
+    password: Joi.string().required().min(8),
+  })
+}), login);
 app.post('/signup', createUser);
 
 app.use(auth);
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
-app.use((req, res) => {
-  res.status(404).send({ message: 'Ошибка 404. Такой страницы не существует' });
-});
+app.use('*', () => {
+  throw new NotFoundError("Такой страницы не существует");
+})
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500 ? 'Произошла ошибка на сервере' : message,
   });
+  next();
 });
 app.listen(PORT);
